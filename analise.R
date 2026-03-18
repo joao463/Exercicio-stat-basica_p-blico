36ab7349c4c195861efca543808150ec2cdfef94
joao463 <joaovitor.ricchini@usp.br>
2026-03-18 01:52:42 -0300

    Atualiza analise.R

--- analise.R
+++ analise.R
@@ -5,3 +5,7 @@ dados <- readRDS("C:/Users/joaov/OneDrive/Documentos/GitHub/pib_cid.RDS")
 str(dados)
 names(dados)
 mean(dados$pib_total, na.rm = TRUE)
+getwd()
+mean(dados$impostos, na.rm = TRUE)
+
+summary(dados$pib_total)
